564a580cd4c83226239bbcca28a619664f5cb575
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2022-05-06 08:58:39 +0200

    opencv without config files

--- src/main.py
+++ src/main.py
@@ -6,10 +6,13 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 from datetime import datetime
+from opencv import get_image_object
 
 app = FastAPI()
 
 MODEL = tf.keras.models.load_model("C:/Users/aritz/OneDrive/Escritorio/MUUUH/3. maila/2/PBL/IA/saved_models/vgg16_weights_full_model")
+
+LABEL_NAMES = ['bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe']
 
 CLASS_NAMES = ['ABBOTTS BABBLER',
  'ABBOTTS BOOBY',
@@ -21,10 +24,6 @@ CLASS_NAMES = ['ABBOTTS BABBLER',
  'GREAT KISKADEE',
  'HOUSE FINCH']
 
-@app.get("/ping")
-async def ping():
-    return "Hello, I am alive"
-
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
@@ -34,19 +33,39 @@ async def predict(
     file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
-    image = tf.image.resize(image, (224, 224))
-    img_batch = np.expand_dims(image, 0)
-    
-    predictions = MODEL.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
+    image, label = get_image_object(image)
+
+    if label == 'bird':
+        image = tf.image.resize(image, (224, 224))
+        img_batch = np.expand_dims(image, 0)
+        
+        predictions = MODEL.predict(img_batch)
+
+        predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+        predicted = True
+        msg = f"{predicted_class} {confidence}"
+
+    elif label in LABEL_NAMES:
+        predicted_class = ''
+        confidence = 0.0
+        predicted = False
+        msg = 'Unknown animal'
+
+    else:
+        predicted_class = ''
+        confidence = 0.0
+        predicted = False
+        msg = 'No animal detected'
     
     return {
         'class': predicted_class,
         'confidence': float(confidence),
         'date': datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
+        'predicted': predicted,
+        'msg': msg
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='localhost', port=8080)
