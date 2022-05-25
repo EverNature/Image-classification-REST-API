ceaaf26154ab35a5f507432a989f80b2f68cb885
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2022-05-25 10:47:04 +0200

    DDOS slowapi request limiter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 from controller.prediction_controller import predict_img
+
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request
+
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
 import uvicorn
 
+
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 origins = [
     "http://localhost",
@@ -19,7 +29,9 @@ app.add_middleware(
 )
 
 @app.post("/predict")
+@limiter.limit("5/minute")
 async def predict(
+    request: Request,
     file: UploadFile = File(...)
 ):
     return await predict_img(file=file)
